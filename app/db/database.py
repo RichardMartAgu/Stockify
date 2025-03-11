239f6716930f5d1ec5b3745e919975e4ff586f19
Richard <125735052+RichardMartAgu@users.noreply.github.com>
2025-03-11 22:10:17 +0100

    Base project and connection to DataBase

---
+++ app/db/database.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from app.config.config import settings
+
+SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
+
+Base = declarative_base()
+Base.metadata.create_all(bind=engine)
+
+try:
+    with engine.connect() as connection:
+        print("Successful connection to the database!")
+except Exception as e:
+    print(f"Error connecting to the database: {e}")
+
+
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
