4016d808c9daf3596b39f0a9c9b4160e5e32ba1a
Richard <125735052+RichardMartAgu@users.noreply.github.com>
2025-03-15 22:47:08 +0100

    add table User with model, repository, router y schema

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -2,26 +2,35 @@ from typing import Optional
 
 from pydantic import BaseModel, Field, EmailStr
 
+example_name = "Pepe"
+example_password = "123456"
+example_email = "pepe@gmail.com"
+example_role = "Admin"
+example_image_url = "https://stockifystorage.s3.us-east-1.amazonaws.com/user_profiles/Flux_Dev_A_stylized_icon_for_a_modern_storage_company_featurin_1.jpeg"
+
 
 class UserResponseSchema(BaseModel):
     id: Optional[int] = Field(examples=[5])
-    username: str = Field(..., examples=["Pepe"])
-    email: EmailStr = Field(..., examples=["pepe@gmail.com"])
-    role: str = Field(..., examples=["Admin"])
+    username: str = Field(examples=[example_name])
+    email: EmailStr = Field(examples=[example_email])
+    role: str = Field(examples=[example_role])
+    image_url: Optional[str] = Field(examples=[example_image_url])
     admin_id: Optional[int] = Field(examples=[3])
 
 
 class CreateUserSchema(BaseModel):
-    username: Optional[str] = Field(..., example="Pepe")
-    password: Optional[str] = Field(..., example="123456")
-    email: Optional[EmailStr] = Field(..., example="pepe@gmail.com")
-    role: str = Field(..., examples=["Admin"])
+    username: str = Field(examples=[example_name])
+    password: str = Field(examples=[example_password])
+    email: EmailStr = Field(examples=[example_email])
+    role: str = Field(examples=[example_role])
+    image_url: Optional[str] = Field(examples=[example_image_url])
     admin_id: Optional[int] = Field(examples=["null"])
 
 
 class UpdateUserSchema(BaseModel):
-    username: Optional[str] = Field(None, example="Pepe")
-    password: Optional[str] = Field(None, example="123456")
-    email: Optional[EmailStr] = Field(None, example="pepe@gmail.com")
-    role: str = Field(None, examples=["Admin"])
-    admin_id: Optional[int] = Field(examples=["null"])
+    username: Optional[str] = Field(None, examples=[example_name])
+    password: Optional[str] = Field(None, examples=[example_password])
+    email: Optional[EmailStr] = Field(None, examples=[example_email])
+    role: Optional[str] = Field(None, examples=[example_role])
+    image_url: Optional[str] = Field(None, examples=[example_image_url])
+    admin_id: Optional[int] = Field(None, examples=["null"])
