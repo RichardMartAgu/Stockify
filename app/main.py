a4e4204c9b6645410da030dac8ce1be4311cbec7
Richard <125735052+RichardMartAgu@users.noreply.github.com>
2025-04-20 11:35:21 +0200

    Create middleware

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
 import uvicorn
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from app.routers import user_router, login_router, warehouse_router, product_router, transaction_router, client_router, \
     alert_router
 
 app = FastAPI(title="Stockify.API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Cambia "*" por los dominios específicos si es necesario
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos los métodos (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],  # Permite todos los headers
+)
 
 # Including routers to handle different endpoints
 app.include_router(user_router.router)
